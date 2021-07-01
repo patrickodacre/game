db8234167310a1addbc0e0ffe85394be478065e2
pwho <patrick@patrickwho.me>
2021-07-01 10:55:03 -0400

    Add VisitorAction Enum to Visitor

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,60 @@
 use std::io::stdin;
 
+#[derive(Debug)]
+enum VisitorAction
+{
+    Accept,
+    AcceptWithNote
+    {
+        note: String,
+    },
+    Refuse,
+    Probation,
+}
+
+#[derive(Debug)]
+struct Visitor
+{
+    name: String,
+    greeting: String,
+    action: VisitorAction,
+    age: u8,
+}
+
+impl Visitor
+{
+    fn new(name: &str, greeting: &str, action: VisitorAction, age: u8) -> Self
+    {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+            action,
+            age,
+        }
+    }
+
+    fn greet(&self) -> &String
+    {
+        &self.greeting
+    }
+}
+
+fn get_name() -> String
+{
+    println!("What's your name?");
+
+    let mut name = String::new();
+
+    stdin().read_line(&mut name).expect("bad input");
+
+    name.trim().to_string()
+}
 fn main()
 {
     let mut visitor_list = vec![
-        Visitor::new("Evan", "Hey, Evan!"),
-        Visitor::new("Christian", "Hey, Christian!"),
-        Visitor::new("Daniel", "Hey, Daniel!"),
+        Visitor::new("Evan", "Hey, Evan!", VisitorAction::Accept, 14),
+        Visitor::new("Christian", "Hey, Christian!", VisitorAction::Refuse, 7),
+        Visitor::new("Daniel", "Hey, Daniel!", VisitorAction::Refuse, 5),
     ];
 
     loop {
@@ -28,42 +77,12 @@ fn main()
                 visitor_list.push(Visitor::new(
                     name.as_str(),
                     format!("Hey {}!", name).as_str(),
+                    VisitorAction::AcceptWithNote {
+                        note: "Some note".to_string(),
+                    },
+                    0,
                 ));
             }
         }
     }
-}
-
-#[derive(Debug)]
-struct Visitor
-{
-    name: String,
-    greeting: String,
-}
-
-impl Visitor
-{
-    fn new(name: &str, greeting: &str) -> Self
-    {
-        Self {
-            name: name.to_lowercase(),
-            greeting: greeting.to_string(),
-        }
-    }
-
-    fn greet(&self) -> &String
-    {
-        &self.greeting
-    }
-}
-
-fn get_name() -> String
-{
-    println!("What's your name?");
-
-    let mut name = String::new();
-
-    stdin().read_line(&mut name).expect("bad input");
-
-    name.trim().to_string()
 }
