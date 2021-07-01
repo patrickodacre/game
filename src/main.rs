f123b0fdbf86097250de88ed01452e362855056e
pwho <patrick@patrickwho.me>
2021-07-01 10:55:03 -0400

    Add VisitorAction Enum to Visitor

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,15 @@
 use std::io::stdin;
 
-fn main()
+#[derive(Debug)]
+enum VisitorAction
 {
-    let mut visitor_list = vec![
-        Visitor::new("Evan", "Hey, Evan!"),
-        Visitor::new("Christian", "Hey, Christian!"),
-        Visitor::new("Daniel", "Hey, Daniel!"),
-    ];
-
-    loop {
-        let name = get_name().to_lowercase();
-
-        if name.is_empty() {
-            println!("Updated list: {:?}\n", visitor_list);
-            println!("Bye!");
-            break;
-        }
-
-        let known_visitor = visitor_list
-            .iter()
-            .find(|v| v.name.to_lowercase().contains(name.as_str()));
-
-        match known_visitor {
-            Some(v) => println!("{}\n", v.greet()),
-            _ => {
-                println!("You're not on the list!\n");
-                visitor_list.push(Visitor::new(
-                    name.as_str(),
-                    format!("Hey {}!", name).as_str(),
-                ));
-            }
-        }
-    }
+    Accept,
+    AcceptWithNote
+    {
+        note: String,
+    },
+    Refuse,
+    Probation,
 }
 
 #[derive(Debug)]
@@ -39,21 +17,36 @@ struct Visitor
 {
     name: String,
     greeting: String,
+    action: VisitorAction,
+    age: u8,
 }
 
 impl Visitor
 {
-    fn new(name: &str, greeting: &str) -> Self
+    fn new(name: &str, greeting: &str, action: VisitorAction, age: u8) -> Self
     {
         Self {
-            name: name.to_lowercase(),
+            name: name.to_string(),
             greeting: greeting.to_string(),
+            action,
+            age,
         }
     }
 
-    fn greet(&self) -> &String
+    fn greet(&self)
     {
-        &self.greeting
+        match &self.action {
+            VisitorAction::Accept => println!("C'mon in!"),
+            VisitorAction::AcceptWithNote { note } => {
+                println!("Welcome, {}", &self.name);
+                println!("{}", note);
+            }
+            VisitorAction::Probation => {
+                println!("Watch yourself, {}, you're on probation.", &self.name);
+            }
+            VisitorAction::Refuse => println!("You're banned, {}. Get out of here.", &self.name),
+            _ => println!("no no"),
+        }
     }
 }
 
@@ -66,4 +59,49 @@ fn get_name() -> String
     stdin().read_line(&mut name).expect("bad input");
 
     name.trim().to_string()
+}
+fn main()
+{
+    let mut visitor_list = vec![
+        Visitor::new("Evan", "Hey, Evan!", VisitorAction::Accept, 14),
+        Visitor::new(
+            "Christian",
+            "Hey, Christian!",
+            VisitorAction::AcceptWithNote {
+                note: "VIP".to_string(),
+            },
+            7,
+        ),
+        Visitor::new("Daniel", "Hey, Daniel!", VisitorAction::Refuse, 5),
+        Visitor::new("Mimi", "Hey, Boo!", VisitorAction::Probation, 42),
+    ];
+
+    loop {
+        let name = get_name();
+
+        if name.is_empty() {
+            println!("Updated list: {:?}\n", visitor_list);
+            println!("Bye!");
+            break;
+        }
+
+        let known_visitor = visitor_list
+            .iter()
+            .find(|v| v.name.to_lowercase().contains(name.to_lowercase().as_str()));
+
+        match known_visitor {
+            Some(v) => v.greet(),
+            _ => {
+                println!("You're not on the list!\n");
+                visitor_list.push(Visitor::new(
+                    name.as_str(),
+                    format!("Hey {}!", name).as_str(),
+                    VisitorAction::AcceptWithNote {
+                        note: "Some note".to_string(),
+                    },
+                    0,
+                ));
+            }
+        }
+    }
 }
