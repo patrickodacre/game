c0a0c6f2ab09ef0b93d7c90b1739e7b4da4dd632
pwho <patrick@patrickwho.me>
2021-07-14 13:30:45 -0400

    Add Camera and Layers for Map and Player

--- src/map.rs
+++ src/map.rs
@@ -40,18 +40,35 @@ impl Map
         }
     }
 
-    pub fn render(&self, ctx: &mut BTerm)
+    pub fn render(&self, ctx: &mut BTerm, camera: &Camera)
     {
-        for y in 0..SCREEN_HEIGHT {
-            for x in 0..SCREEN_WIDTH {
+        ctx.set_active_console(0);
+        for y in camera.top_y..camera.bottom_y {
+            for x in camera.left_x..camera.right_x {
+                if !self.in_bounds(Point::new(x, y)) {
+                    continue;
+                }
+
                 let idx = map_idx(x, y);
 
                 match self.tiles[idx] {
                     TileType::Floor => {
-                        ctx.set(x, y, YELLOW, BLACK, to_cp437('.'));
+                        ctx.set(
+                            x - camera.left_x,
+                            y - camera.top_y,
+                            WHITE,
+                            BLACK,
+                            to_cp437('.'),
+                        );
                     }
                     TileType::Wall => {
-                        ctx.set(x, y, GREEN, BLACK, to_cp437('#'));
+                        ctx.set(
+                            x - camera.left_x,
+                            y - camera.top_y,
+                            WHITE,
+                            BLACK,
+                            to_cp437('#'),
+                        );
                     }
                 }
             }
@@ -61,5 +78,5 @@ impl Map
 
 pub fn map_idx(x: i32, y: i32) -> usize
 {
-    ((y * SCREEN_WIDTH) + x) as usize
+    ((y * DISPLAY_WIDTH) + x) as usize
 }
